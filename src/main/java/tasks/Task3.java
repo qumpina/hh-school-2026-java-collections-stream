507a45938a198cdf240dbf063be16098e81abb1f
Ilya Belinsky <belinskyilya@gmail.com>
2026-02-18 19:26:15 +0300

    Task 3

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,9 +1,12 @@
 package tasks;
 
 import common.Person;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 Задача 3
@@ -12,6 +15,10 @@ import java.util.List;
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    ArrayList<Person> sortedList = persons.stream().sorted(Comparator.comparing(Person::secondName)
+            .thenComparing(Person::firstName)
+            .thenComparing(Person::createdAt))
+        .collect(Collectors.toCollection(ArrayList::new));
+    return sortedList;
   }
 }
