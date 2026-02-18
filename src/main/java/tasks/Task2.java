e3ee45053446e40e1555f338087828b35121492b
Ilya Belinsky <belinskyilya@gmail.com>
2026-02-18 18:26:51 +0300

    Task 2

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,8 +3,10 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 /*
 Задача 2
 На вход принимаются две коллекции объектов Person и величина limit
@@ -16,6 +18,10 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    List<Person> mergedPersons = Stream.concat(persons1.stream(),persons2.stream())
+            .sorted(Comparator.comparing(Person::createdAt))
+            .limit(limit)
+            .collect(Collectors.toCollection(ArrayList::new));
+    return mergedPersons;
   }
 }
